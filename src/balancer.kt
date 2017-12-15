aa9f8dedcd7eaa892ec21a815aa0f0ddce4faddf
Evan <evanmyersflow@gmail.com>
2017-12-14 23:02:04 -0800

    Fixed cations as booleans and recognizing charges

--- src/balancer.kt
+++ src/balancer.kt
@@ -2,38 +2,31 @@ import java.util.*
 
 val periodicTable = listOf("H", "He", "Li", "Be", "B", "C", "N", "O", "F", "Ne", "Na", "Mg", "Al", "S", "Cl", "Ar")
 val scanner = Scanner(System.`in`)
+val inputElement = scanner.nextLine().toUpperCase().trim()
 
 fun main(args: Array<String>) {
-    val element = scanner.nextLine().toUpperCase().trim()
-    if (periodicTable.contains(element)) {
-        when (element) {
-            "Na" -> Cation(true)
-            "Cl" -> Cation(false)
-            else -> throw IllegalArgumentException("That's not an element!")
+
+    if (periodicTable.contains(inputElement)) {
+        val cation: Boolean = when (inputElement) {
+            "Na" -> true
+            "Cl" -> false
+
+            else -> throw IllegalArgumentException("That's not an Element!")
+
         }
+        if (periodicTable.contains((inputElement))) {
+            var charge = when (inputElement) {
+                "Na" -> 1
+                "Cl" -> 1
+
+                else -> throw IllegalArgumentException("That's not an element!")
+            }
+            if (cation) {
+            } else charge *= -1
+        }
+
+
     } else {
-        println("That's not an element!")
+        println("That's not an Element!")
     }
-    if (element.isCation) {
-        when (element) {
-            "Na" -> 1
-            "Cl" -> 1
-        }
-    }
-    fun chargeMath() {
-        if (element.isCation) {
-            Int
-        } else {
-
-        }
-    }
-
 }
-
-interface chargeStrength {
-    val isCation: Boolean
-    val charge: Int
-}
-
-data class Cation(val isCation: Boolean)
-
