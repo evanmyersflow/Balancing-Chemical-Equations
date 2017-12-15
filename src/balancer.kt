2a306ad698805b35403fc5a57f155c246c5d3096
Evan <evanmyersflow@gmail.com>
2017-12-14 23:22:26 -0800

    Updated periodic table, charges, and charge strengths

--- src/balancer.kt
+++ src/balancer.kt
@@ -1,22 +1,44 @@
 import java.util.*
 
-val periodicTable = listOf("H", "He", "Li", "Be", "B", "C", "N", "O", "F", "Ne", "Na", "Mg", "Al", "S", "Cl", "Ar")
+val ionicPeriodicTable = listOf("H", "Li", "Be", "B", "N", "O", "F", "Na", "Mg", "Al", "P", "S", "Cl")
 val scanner = Scanner(System.`in`)
 val inputElement = scanner.nextLine().toUpperCase().trim()
 
 fun main(args: Array<String>) {
 
-    if (periodicTable.contains(inputElement)) {
+    if (ionicPeriodicTable.contains(inputElement)) {
         val cation: Boolean = when (inputElement) {
+            "H" -> true
+            "Li" -> true
+            "Be" -> true
+            "B" -> true
+            "N" -> false
+            "O" -> false
+            "F" -> false
             "Na" -> true
+            "Mg" -> true
+            "Al" -> true
+            "P" -> false
+            "S" -> false
             "Cl" -> false
 
             else -> throw IllegalArgumentException("That's not an Element!")
 
         }
-        if (periodicTable.contains((inputElement))) {
+        if (ionicPeriodicTable.contains((inputElement))) {
             var charge = when (inputElement) {
+                "H" -> 1
+                "Li" -> 1
+                "Be" -> 2
+                "B" -> 3
+                "N" -> 3
+                "O" -> 2
+                "F" -> 1
                 "Na" -> 1
+                "Mg" -> 2
+                "Al" -> 3
+                "P" -> 3
+                "S" -> 2
                 "Cl" -> 1
 
                 else -> throw IllegalArgumentException("That's not an element!")
